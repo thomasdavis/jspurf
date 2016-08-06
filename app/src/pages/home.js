a3d463e4021327fb23191884d24e0d3af747eb30
harmonicageek <ishan.bakshi@gmail.com>
2016-08-06 19:20:17 +1000

    update react ui to cols and rows

--- app/src/pages/home.js
+++ app/src/pages/home.js
@@ -7,20 +7,17 @@ export default class HomePage extends Component {
   render () {
     return (
         <div className="theme-showcase" role="main">
-          <div className="well">
-            <div ng-controller="codeCtrl">
-              <textarea ui-codemirror ng-model="code" className="code-block"></textarea>
-            </div>
+          <div className="well row">
+              <div className="col-md-8"><textarea ui-codemirror className="code-block "></textarea></div>
+              <div className="result-block col-md-3"><p>wewooowjajaj</p></div>
           </div>
-          <div className="well">
-            <div ng-controller="codeCtrl">
-              <textarea ui-codemirror ng-model="code" className="code-block"></textarea>
-            </div>
+          <div className="well row">
+              <div className="col-md-8"><textarea ui-codemirror className="code-block "></textarea></div>
+              <div className="result-block col-md-3"><p>wewooowjajaj</p></div>
           </div>
-          <div className="well">
-            <div ng-controller="codeCtrl">
-              <textarea ui-codemirror ng-model="code" className="code-block"></textarea>
-            </div>
+          <div className="well row">
+              <div className="col-md-8"><textarea ui-codemirror className="code-block "></textarea></div>
+              <div className="result-block col-md-3"><p>wewooowjajaj</p></div>
           </div>
 
       </div>
